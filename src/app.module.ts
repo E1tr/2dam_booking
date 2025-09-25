a6348424b3d454b32dc95ac141310dc2f2668d31
E1tr <gastian02@gmail.com>
2025-09-25 16:33:06 +0200

    v2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+
+    })
+  ]
+})
+@Module({
+  imports: [UsersModule],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
